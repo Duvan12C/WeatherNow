7da975222cc4e666ccec7ef780b436155b634995
Duvan12C <iduvangonzalez.13@gmail.com>
2025-04-24 00:00:57 -0600

    implementacin de log en el controller

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -11,9 +11,11 @@ namespace Api.Controllers
     public class WeatherController : ControllerBase
     {
         private readonly IWeatherService _weatherService;
+        private readonly ILogger<WeatherController> _logger;
 
-        public WeatherController(IWeatherService weatherService)
+        public WeatherController(ILogger<WeatherController> logger, IWeatherService weatherService)
         {
+            _logger = logger;
             _weatherService = weatherService;
         }
 
@@ -21,13 +23,17 @@ namespace Api.Controllers
         public async Task<ActionResult<ResponseWeatherApiExternalDto>> GetCurrentWeather([FromQuery] double lat, [FromQuery] double lon)
 
         {
+            _logger.LogInformation("Recibiendo solicitud GET /weather/current?lat={Lat}&lon={Lon}", lat, lon);
+
             try
             {
                 var weatherData = await _weatherService.GetCurrentWeatherAsync(lat, lon);
+                _logger.LogInformation("Datos obtenidos exitosamente para {Lat},{Lon}", lat, lon);
                 return Ok(weatherData);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error al obtener clima actual para {Lat},{Lon}", lat, lon);
                 return StatusCode(500, new { message = "Ocurrió un error al obtener el clima actual.", details = ex.Message });
             }
         }
@@ -37,13 +43,18 @@ namespace Api.Controllers
         public async Task<ActionResult<ResponseWeatherForecastDto>> GetWeatherForecastAsync([FromQuery] double lat, [FromQuery] double lon)
 
         {
+            _logger.LogInformation("Recibiendo solicitud GET /weather/forecast?lat={Lat}&lon={Lon}&day=7", lat, lon);
+
             try
             {
                 var weatherData = await _weatherService.GetWeatherForecastAsync(lat, lon);
+                _logger.LogInformation("Datos obtenidos exitosamente para  forecast 7 dias {Lat},{Lon}", lat, lon);
+
                 return Ok(weatherData);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error al obtener clima actual para forecast 7 {Lat},{Lon}", lat, lon);
                 return StatusCode(500, new { message = "Ocurrió un error al obtener el clima actual.", details = ex.Message });
             }
         }
